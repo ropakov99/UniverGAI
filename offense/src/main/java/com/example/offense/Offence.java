fa5ff55b9ba3b0cbf98a038ec5ee117ecbde79a1
ARopakov <ARopakov@arlo.com>
2021-04-11 12:34:40 +0300

    update offence api

--- offense/src/main/java/com/example/offense/Offence.java
+++ offense/src/main/java/com/example/offense/Offence.java
@@ -1,7 +1,11 @@
 package com.example.offense;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.Entity;
@@ -10,19 +14,59 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Data
 @Entity
 @Table(name = "offence")
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Offence {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long id;
+    private long id;
 
-    long userId;
+    private long userId;
 
-    String description;
+    private String description;
 
-    long price;
+    private long price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public Offence() {
+    }
+
+    public Offence(long id, long userId, String description, long price) {
+        this.id = id;
+        this.userId = userId;
+        this.description = description;
+        this.price = price;
+    }
 }
--- offense/src/main/java/com/example/offense/OffenceController.java
+++ offense/src/main/java/com/example/offense/OffenceController.java
@@ -1,10 +1,8 @@
 package com.example.offense;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
