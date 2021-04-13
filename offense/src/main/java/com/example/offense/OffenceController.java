3866ae6ccde3220efa4d00a8a6b85937c8af369b
ARopakov <ARopakov@arlo.com>
2021-04-13 21:11:15 +0300

    update

--- offense/src/main/java/com/example/offense/OffenceController.java
+++ offense/src/main/java/com/example/offense/OffenceController.java
@@ -4,6 +4,7 @@ import com.example.offense.model.Offence;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/offence")
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@CrossOrigin(origins = "http://localhost:8080")
 public class OffenceController {
 
     @Autowired
--- user/src/main/java/security/user/UserController.java
+++ user/src/main/java/security/user/UserController.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@CrossOrigin(origins = "http://localhost:8080")
 public class UserController {
 
     @Autowired
@@ -33,6 +35,7 @@ public class UserController {
     }
 
     @DeleteMapping(value = "{id}")
+    @CrossOrigin
     public void deleteUserById(@PathVariable(name = "id") Long id){
         userService.deleteUserById(id);
     }
